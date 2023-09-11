c246c70d6107ffb8c900c76ff72c5fa2538c3f04
mrhankey261993 <mrhankey261993@gmail.com>
2023-09-11 14:29:56 +0300

    +

--- controllers/rules.php
+++ controllers/rules.php
@@ -9,13 +9,15 @@ $api = [
             'GET content' => 'content',
             'POST login' => 'login',
             'POST slide' => 'slide',
+            'OPTIONS slide' => 'options',
             'POST works' => 'works',
+            'OPTIONS works' => 'options',
             'DELETE slide' => 'remove-slide',
             'DELETE service' => 'remove-service',
             'DELETE works' => 'remove-work',
             'POST service' => 'service',
+            'OPTIONS service' => 'options',
             'POST form' => 'form',
-            'OPTIONS <action:\w+>' => 'options'
         ],
     ],
     [
