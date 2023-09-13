4be1614ab2286d4d83145b53166d41709f7e462d
mrhankey261993 <mrhankey261993@gmail.com>
2023-09-13 19:39:35 +0300

    +

--- config/db.php
+++ config/db.php
@@ -5,8 +5,7 @@ return [
     'dsn' => 'mysql:host=31.172.78.63;dbname=admin_elvira',
     'username' => 'admin_root',
     'password' => 'WoyxpWC9',
-    'charset'   => 'utf8mb4',              // **for emoticons**
-    'collation' => 'utf8mb4_unicode_ci',
+    'charset'   => 'utf8mb4',
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
