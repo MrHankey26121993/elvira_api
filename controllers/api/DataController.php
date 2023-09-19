91db04cd19a3108b5853d4961050cee0f2c1c841
MrHankey26121993 <mrhankey261993@gmail.com>
2023-09-19 11:17:46 +0300

    +

--- controllers/api/DataController.php
+++ controllers/api/DataController.php
@@ -47,6 +47,13 @@ class DataController extends Controller
         ];
     }
 
+    public static function allowedDomains() {
+        return [
+             '*',                        // star allows all domains
+            'https://elvirabeauty.fr/',
+        ];
+    }
+
     public function behaviors()
     {
         $behaviors = parent::behaviors();
@@ -62,25 +69,14 @@ class DataController extends Controller
         // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
 
         $behaviors['corsFilter'] = [
-            'class' => \yii\filters\Cors::class,
-
-            'cors' => [
-
-                'Origin' => ['*', 'https://elvirabeauty.fr'],
-
-           //     'Access-Control-Allow-Origin' => ['*', 'https://10.10.10.79:8080','http://localhost:19006'],
-
-                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-
-                'Access-Control-Request-Headers' => ['*'],
-
-                'Access-Control-Allow-Credentials' => null,
-
-                'Access-Control-Max-Age' => 86400,
-
-                'Access-Control-Expose-Headers' => []
-
-            ]
+            'class' => \yii\filters\Cors::className(),
+            'cors'  => [
+                // restrict access to domains:
+                'Origin'                           => static::allowedDomains(),
+                'Access-Control-Request-Method'    => ['POST'],
+                'Access-Control-Allow-Credentials' => true,
+                'Access-Control-Max-Age'           => 3600,                 // Cache (seconds)
+            ],
         ];
         unset($behaviors['rateLimiter']);
 
