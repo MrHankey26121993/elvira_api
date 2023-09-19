bed0f69ce00b2f94e2ffc84871d4d42467b3844c
MrHankey26121993 <mrhankey261993@gmail.com>
2023-09-19 12:14:34 +0300

    +

--- controllers/api/DataController.php
+++ controllers/api/DataController.php
@@ -60,16 +60,9 @@ class DataController extends Controller
             'cors' => [
                 // restrict access to
                 'Origin' => ['*'],
-                'Access-Control-Allow-Origin' => ['*'],
                 'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-                // Allow only POST and PUT methods
-                'Access-Control-Request-Headers' => ['*'],
-                // Allow only headers 'X-Wsse'
                 'Access-Control-Allow-Credentials' => true,
-                // Allow OPTIONS caching
-                'Access-Control-Max-Age' => 86400,
-                // Allow the X-Pagination-Current-Page header to be exposed to the browser.
-                'Access-Control-Expose-Headers' => [],
+                'Access-Control-Max-Age'           => 3600,
             ]
         ];
 
