22d2165f162819f19cbc3371162725f1fef756ee
MrHankey26121993 <mrhankey261993@gmail.com>
2023-09-19 14:32:36 +0300

    +

--- controllers/api/DataController.php
+++ controllers/api/DataController.php
@@ -38,60 +38,42 @@ class DataController extends Controller
 
 
     public function actions()
-
-
     {
-
-        $actions = parent::actions();
-        $actions['options'] = [
-            'class' => 'yii\rest\OptionsAction',
-            // optional:
-            'collectionOptions' => ['GET', 'POST', 'HEAD', 'OPTIONS'],
-            'resourceOptions' => ['GET', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+        return [
+            'options' => [
+                'class' => 'yii\rest\OptionsAction',
+                'collectionOptions' => ['GET', 'POST', 'HEAD', 'OPTIONS'],
+                'resourceOptions' => ['GET', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+            ],
         ];
-        return $actions;
     }
 
     public function behaviors()
     {
         $behaviors = parent::behaviors();
 
+        // remove authentication filter
+        $auth = $behaviors['authenticator'] = [
+            'class' => Bearer::className(),
+        ];
 
         unset($behaviors['authenticator']);
 
         $behaviors['corsFilter'] = [
-            'class' => \yii\filters\Cors::class,
-            'cors' => [
-                'Origin' => [
-                    '*',
-                ],
-                'Access-Control-Request-Method' => [
-                    'GET',
-                    'POST',
-                    'PUT',
-                    'PATCH',
-                    'DELETE',
-                    'HEAD',
-                    'OPTIONS',
-                ],
-                'Access-Control-Request-Headers' => [
-                    '*',
-                ],
-                'Access-Control-Allow-Credentials' => true,
-                'Access-Control-Max-Age' => 86400,
-                'Access-Control-Expose-Headers' => [],
-            ],
+            'class' => \yii\filters\Cors::class
         ];
+        $behaviors['authenticator'] = $auth;
+        // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
 
-        $behaviors['authenticator'] = [
-            'class' => Bearer::class,
-            'except' => [
-                'options',
-                'login',
-                'index',
-                'content',
-                'form',
-            ]
+
+        unset($behaviors['rateLimiter']);
+
+        $behaviors['authenticator']['except'] = [
+            'login',
+            'index',
+            'content',
+            'form',
+            'options',
         ];
 
 
@@ -145,9 +127,9 @@ class DataController extends Controller
 
 
         } catch (yii\web\HttpException $e) {
-            return $e;
+            return Util::returnInfo($e, 'authorization_error', true);
         } catch (\ErrorException $e) {
-            return $e;
+            return Util::returnInfo([], 'authorization_error', true);
         }
     }
 
