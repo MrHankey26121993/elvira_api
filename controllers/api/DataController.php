81f8655db1db7283e8d37daf8bd4d0e1f6c6fc67
MrHankey26121993 <mrhankey261993@gmail.com>
2023-09-19 12:17:36 +0300

    +

--- controllers/api/DataController.php
+++ controllers/api/DataController.php
@@ -65,6 +65,7 @@ class DataController extends Controller
             'cors' => [
                 // restrict access to
                 'Origin' => ['*'],
+                'Access-Control-Allow-Origin' => ['*'],
                 'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
                 'Access-Control-Allow-Credentials' => true,
                 'Access-Control-Max-Age'           => 3600,
