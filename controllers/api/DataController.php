52be274fd84f972c8bb2182b08a099fa652db1aa
MrHankey26121993 <mrhankey261993@gmail.com>
2023-09-19 12:09:55 +0300

    +

--- controllers/api/DataController.php
+++ controllers/api/DataController.php
@@ -55,14 +55,28 @@ class DataController extends Controller
             'class' => Bearer::className(),
         ];
 
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::className(),
+            'cors' => [
+                // restrict access to
+                'Access-Control-Allow-Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                // Allow only POST and PUT methods
+                'Access-Control-Request-Headers' => ['*'],
+                // Allow only headers 'X-Wsse'
+                'Access-Control-Allow-Credentials' => true,
+                // Allow OPTIONS caching
+                'Access-Control-Max-Age' => 86400,
+                // Allow the X-Pagination-Current-Page header to be exposed to the browser.
+                'Access-Control-Expose-Headers' => [],
+            ]
+        ];
+
         unset($behaviors['authenticator']);
 
         $behaviors['authenticator'] = $auth;
         // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
 
-        $behaviors['corsFilter'] = [
-            'class' => \yii\filters\Cors::class
-        ];
         unset($behaviors['rateLimiter']);
 
         $behaviors['authenticator']['except'] = [
