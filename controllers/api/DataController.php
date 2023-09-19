36fa806fe98a38d6be98976f6030d9204aeeac39
MrHankey26121993 <mrhankey261993@gmail.com>
2023-09-19 10:28:19 +0300

    +

--- controllers/api/DataController.php
+++ controllers/api/DataController.php
@@ -61,7 +61,25 @@ class DataController extends Controller
         // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
 
         $behaviors['corsFilter'] = [
-            'class' => \yii\filters\Cors::class
+            'class' => \yii\filters\Cors::class,
+
+            'cors' => [
+
+                'Origin' => ['*'],
+
+                'Access-Control-Allow-Origin' => ['*'],
+
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+
+                'Access-Control-Request-Headers' => ['*'],
+
+                'Access-Control-Allow-Credentials' => null,
+
+                'Access-Control-Max-Age' => 86400,
+
+                'Access-Control-Expose-Headers' => []
+
+            ]
         ];
         unset($behaviors['rateLimiter']);
 
