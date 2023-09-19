146e3b81c9361462d592fe20a6b6b31132c1dffb
MrHankey26121993 <mrhankey261993@gmail.com>
2023-09-19 14:06:21 +0300

    +

--- controllers/api/DataController.php
+++ controllers/api/DataController.php
@@ -35,14 +35,7 @@ class DataController extends Controller
         $this->param = \Yii::$app->request->post() ?: \Yii::$app->request->get();
     }
 
-    public function beforeAction($action)
-    {
-        if (parent::beforeAction($action)) {
-            \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
-            return true;
-        }
 
-    }
 
     public function actions()
 
